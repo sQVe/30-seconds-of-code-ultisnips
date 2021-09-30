c7afe707234b432c0a99b5e19010264f143914ee
Oskar Grunning <oskargrunning@gmail.com>
2021-09-30 16:00:23 +0200

    chore: update dependencies

--- src/parser.js
+++ src/parser.js
@@ -1,47 +1,46 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
-const commaRe = /,+/g
-const identifierKeyRe = /^(title|tags):\s+/
-const newlineRe = /\n+/g
-const sectionRe = /---|```(?:js)?/g
-const surroundingWhitespaceRe = /^\s+|\s+$/g
-const parsableCharsRe = /(\${|`)/g
+const commaRe = /,+/g;
+const identifierKeyRe = /^(title|tags):\s+/;
+const newlineRe = /\n+/g;
+const sectionRe = /---|```(?:js)?/g;
+const surroundingWhitespaceRe = /^\s+|\s+$/g;
+const parsableCharsRe = /(\${|`)/g;
 
 const parseSections = (content) =>
   content
     .split(sectionRe)
     .map((x) => x.replace(surroundingWhitespaceRe, ''))
-    .filter(Boolean)
+    .filter(Boolean);
 
 const sanitizeIdentifiers = (identifiers) => {
   const [id, tags] = identifiers
     .split(newlineRe)
-    .map((x) => x.replace(identifierKeyRe, ''))
+    .map((x) => x.replace(identifierKeyRe, ''));
 
-  return [id, tags.split(commaRe)]
-}
+  return [id, tags.split(commaRe)];
+};
 
-const sanitizeCode = (code) => code.replace(parsableCharsRe, '\\$1')
+const sanitizeCode = (code) => code.replace(parsableCharsRe, '\\$1');
 
 const sourceDirectory = path.join(
   __dirname,
   '../submodules/30-seconds-of-code/snippets'
-)
-const outputPath = path.join(__dirname, '../dist/snippets.json')
+);
+const outputPath = path.join(__dirname, '../dist/snippets.json');
 
 const sourceFiles = fs
   .readdirSync(sourceDirectory)
-  .map((x) => path.join(sourceDirectory, x))
+  .map((x) => path.join(sourceDirectory, x));
 
 const snippets = sourceFiles.reduce((acc, file) => {
-  const sections = parseSections(fs.readFileSync(file, { encoding: 'utf8' }))
+  const sections = parseSections(fs.readFileSync(file, { encoding: 'utf8' }));
 
-  const [identifiers, description, code, example] = sections
-  const [id, tags] = sanitizeIdentifiers(identifiers)
+  const [identifiers, description, code, example] = sections;
+  const [id, tags] = sanitizeIdentifiers(identifiers);
 
-  return [...acc, { code: sanitizeCode(code), description, example, id, tags }]
-}, [])
+  return [...acc, { code: sanitizeCode(code), description, example, id, tags }];
+}, []);
 
-// eslint-disable-next-line fp/no-nil
-fs.writeFileSync(outputPath, JSON.stringify(snippets, null, 2))
+fs.writeFileSync(outputPath, JSON.stringify(snippets, null, 2));
